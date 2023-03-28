24a902f773430f9a809ccee5f26813b2c2ee32bb
Josh Merlino <joshua.merlino@gmail.com>
2023-03-28 14:42:16 -0400

    clean up connection status jawn

--- src/renderer/components/ConnectionStatus.tsx
+++ src/renderer/components/ConnectionStatus.tsx
@@ -36,10 +36,10 @@ export default function ConnectionStatus(): JSX.Element | null {
 
 	return (
 		<div className="group relative">
-			<div className={ classNames("w-12 h-12 -m-4 rounded-full flex items-center justify-center hover:bg-gray-500/10 hover:active:bg-gray-500/20 transition-colors border", isConnected ? "border-success/25" : "border-error/25") }>
+			<div className={ classNames("w-12 h-12 -m-4 rounded-full flex items-center justify-center hover:bg-gray-500/10 hover:active:bg-gray-500/20 transition-colors border", isConnected ? "border-success/25 bg-success/10" : "border-error/25 bg-error/10") }>
 				<Icon />
 			</div>
-			<div className={ classNames("group-hover:scale-100 scale-75 opacity-0 group-hover:opacity-100 transition-all absolute px-2 py-0.5 text-sm origin-top-left rounded-lg top-0 pointer-events-none shadow-xl border dark:border-gray-700/50 text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden -m-4 dark:shadow-black/20 flex flex-col !p-4 gap-3 group-hover:pointer-events-auto min-w-[288px] after:content-[''] after:absolute after:top-0 after:left-0 after:bottom-0 after:right-0 after:-z-[1]", isConnected ? "!border-success/25 after:bg-success/10" : "!border-error/25 after:bg-error/10") }>
+			<div className={ classNames("group-hover:scale-100 scale-75 opacity-0 group-hover:opacity-100 transition-all absolute px-2 py-0.5 text-sm origin-top-left rounded-lg top-0 pointer-events-none shadow-xl border dark:border-gray-700/50 text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden -m-4 dark:shadow-black/20 flex flex-col !p-3.5 gap-3 group-hover:pointer-events-auto min-w-[288px] after:content-[''] after:absolute after:top-0 after:left-0 after:bottom-0 after:right-0 after:pointer-events-none", isConnected ? "!border-success/25 after:bg-success/10" : "!border-error/25 after:bg-error/10 rounded-[24px] group-hover:rounded-md") }>
 				<div className="flex items-center gap-4">
 					<div>
 						<Icon />
