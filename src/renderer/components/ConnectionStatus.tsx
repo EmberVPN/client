d28967fa46f2105ef73a5caa18a3c7b8c9c11f2f
Josh Merlino <joshua.merlino@gmail.com>
2023-03-28 14:36:16 -0400

    more UI touchups

--- src/renderer/components/ConnectionStatus.tsx
+++ src/renderer/components/ConnectionStatus.tsx
@@ -22,7 +22,11 @@ export default function ConnectionStatus(): JSX.Element | null {
 		setIsLoading(false);
 	}, [ ipLocation ]);
 
-	if (!ipLocation || !servers) return null;
+	if (!ipLocation || !servers) return (
+		<div className="w-12 h-12 -m-4 rounded-full flex items-center justify-center hover:bg-gray-500/10 hover:active:bg-gray-500/20 transition-colors border border-amber-500/25">
+			<Spinner className="mx-2 !stroke-amber-500" />
+		</div>
+	);
 	
 	const isConnected = Object.values(servers.servers).some(server => server.ip === ipLocation.ip);
 
@@ -35,7 +39,7 @@ export default function ConnectionStatus(): JSX.Element | null {
 			<div className={ classNames("w-12 h-12 -m-4 rounded-full flex items-center justify-center hover:bg-gray-500/10 hover:active:bg-gray-500/20 transition-colors border", isConnected ? "border-success/25" : "border-error/25") }>
 				<Icon />
 			</div>
-			<div className={ classNames("group-hover:scale-100 scale-75 opacity-0 group-hover:opacity-100 transition-all absolute px-2 py-0.5 text-sm origin-top-left rounded-lg top-0 pointer-events-none shadow-xl border dark:border-gray-700/50 text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden -m-4 dark:shadow-black/20 flex flex-col !p-4 gap-3 group-hover:pointer-events-auto min-w-[288px]", isConnected ? "border-success/25" : "border-error/25") }>
+			<div className={ classNames("group-hover:scale-100 scale-75 opacity-0 group-hover:opacity-100 transition-all absolute px-2 py-0.5 text-sm origin-top-left rounded-lg top-0 pointer-events-none shadow-xl border dark:border-gray-700/50 text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden -m-4 dark:shadow-black/20 flex flex-col !p-4 gap-3 group-hover:pointer-events-auto min-w-[288px] after:content-[''] after:absolute after:top-0 after:left-0 after:bottom-0 after:right-0 after:-z-[1]", isConnected ? "!border-success/25 after:bg-success/10" : "!border-error/25 after:bg-error/10") }>
 				<div className="flex items-center gap-4">
 					<div>
 						<Icon />
