0a49629f73cd0088b18edd066d08da34a9c0c738
Josh Merlino <joshua.merlino@gmail.com>
2023-03-28 03:29:43 -0400

    fix...

--- src/renderer/components/Server.tsx
+++ src/renderer/components/Server.tsx
@@ -105,58 +105,13 @@ shadow-xl border dark:border-gray-700/50 text-gray-600 bg-white dark:bg-gray-800
 				</div>
 				{ !isConnected && <Button className={ classNames(isLoading && "!bg-opacity-25 !shadow-none pointer-events-none") }
 					color="primary"
-					onClick={ disconnect }>{isLoading ? <Spinner className="w-6 mx-auto !stroke-primary" /> : "Connect"}</Button>
+					onClick={ connect }>{isLoading ? <Spinner className="w-6 mx-auto !stroke-primary" /> : "Connect"}</Button>
 				}
 				{ isConnected && <Button className={ classNames(isLoading && "!bg-opacity-25 !shadow-none pointer-events-none") }
 					color="error"
 					onClick={ disconnect }>{isLoading ? <Spinner className="w-6 mx-auto !stroke-error" /> : "Disconnect"}</Button>
 				}
 			</div>
-		</div>
-	);
-
-	return (
-		<div className="absolute group hover:z-[1]"
-			style={{
-				bottom: `${ (latitude + 90) / 180 * 100 }%`,
-				left: `${ (longitude + 180) / 360 * 100 }%`,
-			}}>
-			
-			<div className="w-14 aspect-square -translate-x-1/2 rounded-full">
-				<MdLocationPin className="text-primary text-6xl group-hover:scale-150 origin-bottom opacity-60 group-hover:opacity-100 transition-[opacity,transform]" />
-			</div>
-
-			<div className="absolute opacity-0 pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100 -translate-x-1/2 transition-[opacity,transform] scale-75 group-hover:scale-100 origin-top w-fit">
-				<div className="bg-gray-100 dark:bg-gray-800 rounded-lg p-4 shadow-xl flex flex-col min-w-[360px] gap-4">
-					
-					<div className="flex items-center gap-4">
-						<img
-							className="rounded"
-							src={ `https://my.vultr.com/_images/flags/flagsm_${ server.location.country_code2.toLowerCase() }.png` } />
-						
-						<div className="flex flex-col w-full">
-							<div className="flex flex-nowrap justify-between text-xl font-medium whitespace-nowrap">
-								<h1 className="grow">{server.location.country_name}</h1>
-								<p className="text-gray-500 font-medium font-mono">{server.hostname.split("-")[1]}</p>
-							</div>
-							<div className="flex flex-nowrap gap-2 items-center text-gray-700 dark:text-gray-400 font-medium text-sm">
-								<span>{ Intl.NumberFormat().format(Math.floor(distance)) }km</span>
-								<span>•</span>
-								<span>{server.ping}ms</span>
-								<span className="grow"></span>
-								<span>{server.ip}</span>
-							</div>
-						</div>
-					</div>
-
-					{ !isConnected && <button className="h-12 text-primary hover:bg-primary/10 bg-primary/5 text-lg font-medium rounded-lg"
-						onClick={ connect }>{isLoading ? <Spinner className="w-5 mx-auto !stroke-primary" /> : "Quick Connect"}</button>}
-					{isConnected && <button className="h-12 bg-red-600 hover:bg-red-700 text-white text-lg font-medium rounded-lg"
-						onClick={ disconnect }>{isLoading ? <Spinner className="w-5 mx-auto !stroke-white" /> : "Disconnect"}</button>}
-
-				</div>
-			</div>
-
 		</div>
 	);
 }
