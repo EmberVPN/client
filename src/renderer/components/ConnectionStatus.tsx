1785f44580649dc33c438f379e25e7f9f714c18e
Josh Merlino <joshua.merlino@gmail.com>
2023-03-28 03:46:20 -0400

    ui touchups

--- src/renderer/components/ConnectionStatus.tsx
+++ src/renderer/components/ConnectionStatus.tsx
@@ -35,8 +35,7 @@ export default function ConnectionStatus(): JSX.Element | null {
 			<div className={ classNames("w-12 h-12 -m-4 rounded-full flex items-center justify-center hover:bg-gray-500/10 hover:active:bg-gray-500/20 transition-colors border", isConnected ? "border-success/25" : "border-error/25") }>
 				<Icon />
 			</div>
-			<div className="group-hover:scale-100 scale-75 opacity-0 group-hover:opacity-100 transition-all absolute px-2 py-0.5 text-sm origin-top-left rounded-lg top-0 pointer-events-none
-shadow-xl border dark:border-gray-700/50 text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden -m-4 dark:shadow-black/20 flex flex-col !p-4 gap-3 group-hover:pointer-events-auto min-w-[288px]">
+			<div className={ classNames("group-hover:scale-100 scale-75 opacity-0 group-hover:opacity-100 transition-all absolute px-2 py-0.5 text-sm origin-top-left rounded-lg top-0 pointer-events-none shadow-xl border dark:border-gray-700/50 text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden -m-4 dark:shadow-black/20 flex flex-col !p-4 gap-3 group-hover:pointer-events-auto min-w-[288px]", isConnected ? "border-success/25" : "border-error/25") }>
 				<div className="flex items-center gap-4">
 					<div>
 						<Icon />
