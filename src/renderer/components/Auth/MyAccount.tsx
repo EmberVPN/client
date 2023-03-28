ca3e0c98db4d133db84300c1b571ae883a7c2905
Josh Merlino <joshua.merlino@gmail.com>
2023-03-27 20:08:09 -0400

    work on light theme

--- src/renderer/components/Auth/MyAccount.tsx
+++ src/renderer/components/Auth/MyAccount.tsx
@@ -14,7 +14,7 @@ export default function MyAccount({ user }: { user: User }): JSX.Element {
 
 	return (
 		<div className={ classNames("group relative", _open && "is-open") }>
-			<button className="flex mx-3 text-sm bg-neutral-500/25 rounded-full md:mr-0 focus:ring-4 focus:ring-primary-400 -my-1 select-none"
+			<button className="flex text-sm bg-neutral-500/25 rounded-full md:mr-0 focus:ring-4 focus:ring-primary-400 -my-1 select-none"
 				onClick={ open }>
 				<img alt="user photo"
 					className="w-9 h-9 rounded-full"
