57fd02204927b996b9668be7f4df930401d29751
Josh Merlino <joshua.merlino@gmail.com>
2023-03-29 22:56:03 -0400

    update tooltip

--- src/renderer/components/Tooltip.tsx
+++ src/renderer/components/Tooltip.tsx
@@ -2,6 +2,6 @@ import { PropsWithChildren } from "react";
 
 export default function Tooltip({ children }: PropsWithChildren) {
 	return (
-		<div className="absolute top-full bg-neutral-700 text-xs font-medium rounded-md h-6 flex items-center px-2 pointer-events-none opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 transition-all origin-top">{children}</div>
+		<div className="absolute top-full text-white bg-neutral-500 dark:bg-neutral-700 text-xs font-medium rounded-md h-6 flex items-center px-2 pointer-events-none opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 transition-all origin-top">{children}</div>
 	);
 }
