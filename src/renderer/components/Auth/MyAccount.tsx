c255a15d2ee451d5939a6c310a07822cd4b2b1b1
Josh Merlino <joshua.merlino@gmail.com>
2023-03-30 00:13:08 -0400

    keep refactoring

--- src/renderer/components/Auth/MyAccount.tsx
+++ src/renderer/components/Auth/MyAccount.tsx
@@ -19,7 +19,7 @@ export default function MyAccount({ user }: { user: User }): JSX.Element {
 			<div className="w-12 h-12 -m-4 rounded-full flex items-center justify-center hover:bg-gray-500/10 hover:active:bg-gray-500/20 transition-colors relative"
 				onClick={ open }>
 				<MdMoreVert className="text-2xl" />
-				<Tooltip>More</Tooltip>
+				<Tooltip anchor="right">More</Tooltip>
 			</div>
 			<MyAccountPopout user={ user } />
 		</div>
