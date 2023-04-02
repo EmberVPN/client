2aa9a284842cbe233be850a24e4c857812f95174
Josh Merlino <joshua.merlino@gmail.com>
2023-04-02 17:54:12 -0400

    add the little eye for my dad

--- src/renderer/components/InputField.tsx
+++ src/renderer/components/InputField.tsx
@@ -1,13 +1,16 @@
 import classNames from "classnames";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 
 interface InputFieldProps {
 	label: string;
 	hint?: string;
 }
 
-export default function InputField({ label, hint, className, ...props }: Partial<InputFieldProps> & React.InputHTMLAttributes<HTMLInputElement>) {
+export default function InputField({ label, hint, className, type, ...props }: Partial<InputFieldProps> & React.InputHTMLAttributes<HTMLInputElement>) {
+
 	const ref = useRef<HTMLDivElement>(null);
+	const [ vis, setVis ] = useState(false);
 	props.id = props.id || Math.floor(Math.random() * 1e10).toString(36);
 
 	useEffect(function() {
@@ -22,10 +25,17 @@ export default function InputField({ label, hint, className, ...props }: Partial
 		<div className={ classNames("relative w-full h-12 group input-group", className) }
 			ref={ ref }>
 			<input className={ classNames("peer w-full h-full text-gray-700 dark:text-gray-200 font-roboto font-normal disabled:border-dashed transition-all placeholder-shown:border placeholder-shown:border-gray-500/20 border focus:border-2 text-sm px-3 py-2.5 focus:px-[11px] rounded-lg border-gray-400/40 dark:border-gray-400/25 focus:!border-primary backdrop-blur-2xl !bg-white/10 dark:!bg-gray-800/10 group-[.hascontents]:invalid:!border-error", className) }
+				type={ type === "password" ? (vis ? "text" : "password") : type || "text" }
 				{ ...props } />
 			{label && (
 				<label className="text-gray-600 dark:text-gray-400 font-roboto font-normal text-sm px-1 absolute top-1/2 left-2 -translate-y-1/2 py-0.5 peer-focus:top-0.5 peer-focus:text-primary peer-focus:text-xs peer-focus:px-2 peer-placeholder-shown:text-xs peer-placeholder-shown:px-2 peer-placeholder-shown:top-0.5 z-[1] transition-all peer-focus:bg-white dark:peer-focus:bg-gray-800 peer-placeholder-shown:bg-white dark:peer-placeholder-shown:bg-gray-800 peer-placeholder-shown:dark:bg-gray-800 group-[.hascontents]:bg-white dark:group-[.hascontents]:bg-gray-800 group-[.hascontents]:text-xs group-[.hascontents]:px-2 group-[.hascontents]:top-0.5 group-[.hascontents]:peer-invalid:!text-error"
 					htmlFor={ props.id }>{label}</label>
+			)}
+			{ type === "password" && (
+				<div className="absolute right-4 top-1/2 -translate-y-1/2 opacity-0 transition-opacity duration-75 group-hover:opacity-100 cursor-pointer"
+					onClick={ () => setVis(vis => !vis) }>
+					{ vis ? <MdVisibilityOff className="text-2xl opacity-70" />	: <MdVisibility className="text-2xl opacity-70" /> }
+				</div>
 			)}
 			{hint && (
 				<div className="absolute px-3 text-sm font-roboto my-0.5 text-gray-500 group-[.hascontents]:peer-invalid:!text-error">{hint}</div>
