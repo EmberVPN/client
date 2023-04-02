445beaba10ea08ec09e1ef62d8e8a7983637cbd4
Josh Merlino <joshua.merlino@gmail.com>
2023-04-01 23:48:01 -0400

    clean up updater a bit

--- src/renderer/components/Updater.tsx
+++ src/renderer/components/Updater.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { MdSystemUpdateAlt } from "react-icons/md";
 import semver from "semver";
 import useData from "../util/hooks/useData";
+import { close } from "./Auth/MyAccount";
 import Button from "./Button";
 
 export default function Updater(): JSX.Element | null {
@@ -13,7 +14,7 @@ export default function Updater(): JSX.Element | null {
 	useEffect(function() {
 		if (!downloads) return;
 		const latest = downloads?.latest[platform];
-		const shouldUpdate = semver.gt(latest?.version, VERSION) && localStorage.getItem("ignoreUpdate") !== latest?.version;
+		const shouldUpdate = semver.gt(latest?.version, DEVELOPMENT ? "0.0.0" : VERSION) && localStorage.getItem("ignoreUpdate") !== latest?.version;
 		if (!state && shouldUpdate) setState(true);
 	}, [ downloads, state ]);
 	
@@ -24,13 +25,15 @@ export default function Updater(): JSX.Element | null {
 	
 	const latest = downloads?.latest[platform] as REST.Version;
 	if (!latest) return (
-		<div className={ classNames("group absolute w-full h-full flex justify-center items-center z-[40] bg-black/20 transition-opacity", state ? "opacity-1 pointer-events-auto active" : "opacity-0 pointer-events-none") }>
-			<div className="shadow dark:shadow-xl bg-white dark:bg-gray-800  dark:shadow-black/20 w-full h-full md:max-w-[600px] md:max-h-[400px] md:rounded-2xl group-[.active]:scale-100 scale-50 transition-transform" />
+		<div className={ classNames("group fixed top-0 w-full h-full flex justify-center items-center z-40 bg-black/20 transition-opacity", state ? "opacity-1 pointer-events-auto active" : "opacity-0 pointer-events-none") }
+			onClick={ close }>
+			<div className="shadow dark:shadow-xl bg-white dark:bg-gray-800 dark:shadow-black/20 w-full h-full md:max-w-[600px] md:max-h-[400px] md:rounded-2xl group-[.active]:scale-100 scale-50 transition-transform" />
 		</div>
 	);
 
 	return (
-		<div className={ classNames("group absolute w-full h-full flex justify-center items-center z-[40] bg-black/20 transition-opacity", state ? "opacity-1 pointer-events-auto active" : "opacity-0 pointer-events-none") }>
+		<div className={ classNames("group fixed top-0 w-full h-full flex justify-center items-center z-40 bg-black/20 transition-opacity", state ? "opacity-1 pointer-events-auto active" : "opacity-0 pointer-events-none") }
+			onClick={ close }>
 			<div className="text-sm shadow dark:shadow-xl text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden dark:shadow-black/20 flex flex-col gap-2 w-full h-full md:max-w-[600px] md:max-h-[400px] md:rounded-2xl group-[.active]:scale-100 scale-50 transition-transform relative p-8">
 
 				<div className="grow flex flex-col gap-4">
