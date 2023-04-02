ab9501f44ad74a5d4f832bc350914bf21cf2d9b9
Josh Merlino <joshua.merlino@gmail.com>
2023-04-02 16:58:20 -0400

    deploy...

--- src/main/updater.ts
+++ src/main/updater.ts
@@ -1,9 +1,9 @@
 import { spawn } from "child_process";
-import { app, BrowserWindow, ipcMain } from "electron";
+import { BrowserWindow, ipcMain } from "electron";
 import { existsSync } from "fs";
 import { writeFile } from "fs/promises";
 import fetch from "node-fetch";
-import { basename, resolve } from "path";
+import { basename, dirname, resolve } from "path";
 import { resources } from ".";
 
 export type State = "begin";
@@ -22,25 +22,24 @@ export default function(win: BrowserWindow) {
 				.then(res => res.json() as Promise<REST.ClientDownloads>);
 			const version = latest[process.platform];
 
-			const path = resolve(resources, `.bin/updater-${ version }.exe`);
-			if (!existsSync(path)) {
-				await fetch(version.download_url)
-					.then(res => res.arrayBuffer())
-					.then(buf => Buffer.from(buf))
-					.then(buf => writeFile(path, buf));
-			}
+			// Download binary
+			const path = resolve(resources, `.bin/updater-${ version.name }`);
+			if (!existsSync(path)) await fetch(version.download_url)
+				.then(res => res.arrayBuffer())
+				.then(buf => Buffer.from(buf))
+				.then(buf => writeFile(path, buf));
 
 			try {
 
 				// Launch binary
-				const child = spawn("cmd", [ "/c", "start", path ], {
-					stdio: "ignore",
-					detached: true
+				const child = spawn(basename(path), {
+					cwd: dirname(path),
+					detached: true,
+					shell: true
 				});
-
+				
+				child.disconnect();
 				child.unref();
-				child.once("spawn", () => spawn("cmd", [ "/c", "taskkill", "/f", "/im", basename(app.getPath("exe")) ])
-					.once("spawn", () => app.quit()));
 
 			} catch (e) {
 				win.webContents.send("updater", "error", e);
