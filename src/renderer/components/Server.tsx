32e5a60e3d9f9b72212d9a9096cb106d4946f58b
Josh Merlino <joshua.merlino@gmail.com>
2023-04-03 18:11:27 -0400

    make server cards more dynamic

--- src/renderer/components/Server.tsx
+++ src/renderer/components/Server.tsx
@@ -19,6 +19,7 @@ export default function Servers({ server }: { server: Ember.Server }): JSX.Eleme
 	// Calculate the distance from the user to the server
 	const distance = calculateDistance(latitude, longitude, ipLocation.latitude, ipLocation.longitude);
 	const isActive = active === server.hash;
+	const isLoading = (status === "connecting" || status === "disconnecting") && isActive;
 
 	// Connect to the server
 	async function connect() {
@@ -36,12 +37,15 @@ export default function Servers({ server }: { server: Ember.Server }): JSX.Eleme
 
 	// Render the server
 	return (
-		<div className="text-sm origin-top rounded-lg shadow text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden dark:shadow-black/20 flex flex-col p-4 gap-2">
+		<div className={ classNames("text-sm rounded-lg shadow text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden dark:shadow-black/20 flex flex-col p-4 gap-2 transition-[height,margin,transform] duration-100", !isLoading ? "h-[134px]" : "h-[88px] my-[23px]", (isLoading || (isActive && status === "connected")) && "scale-110 shadow-lg", (isActive && status === "connected") && "my-2") }>
 				
 			<div className="flex items-center gap-4 p-1">
 
-				<img className="w-12"
-					src={ `https://cdn.ipregistry.co/flags/emojitwo/${ server.location.country_code2.toLowerCase() }.svg` } />
+				<div className="w-12 h-12 relative shrink-0">
+					<img className={ classNames(isLoading && "!opacity-0", "opacity-100 transition-opacity") }
+						src={ `https://cdn.ipregistry.co/flags/emojitwo/${ server.location.country_code2.toLowerCase() }.svg` } />
+					<Spinner className={ classNames("absolute top-0 left-0 !stroke-gray-800 dark:!stroke-gray-200", !isLoading && "!opacity-0", "opacity-100 transition-opacity") } />
+				</div>
 					
 				<div className="leading-tight font-roboto font-medium whitespace-nowrap w-full">
 					<div className="flex w-full justify-between items-center text-gray-700 dark:text-gray-300 text-lg">
@@ -49,22 +53,21 @@ export default function Servers({ server }: { server: Ember.Server }): JSX.Eleme
 						<p>{ server.location.district || server.location.state_prov || server.location.country_capital }</p>
 					</div>
 					<div className="text-sm flex justify-between gap-2 w-full">
-						<p className={ classNames(server.ping < 50 ? "text-success" : server.ping < 150 ? "text-warn" : "text-error") }>{server.ping}ms</p>
 						{ (!isActive || status !== "connected") && (<>
+							<p className={ classNames(server.ping < 50 ? "text-success" : server.ping < 150 ? "text-warn" : "text-error") }>{server.ping}ms</p>
 							<span className="text-gray-400 dark:text-gray-600">•</span>
 							<p>{Intl.NumberFormat().format(Math.floor(distance * (ipLocation.country_code === "US" ? 0.621371 : 1)))} {ipLocation.country_code === "US" ? "Mi" : "Km"}</p>
 							<span className="text-gray-400 dark:text-gray-600">•</span>
 						</>) }
-						<p>{server.location.ip}</p>
+						<p className="ml-auto">{server.location.ip}</p>
 					</div>
 				</div>
 
 			</div>
 
-			{ (isActive && (status === "connecting" || status === "disconnecting")) ? (
-				<Button className="m-0 !bg-transparent pointer-events-none"
+			{isLoading ? (
+				<Button className="!bg-transparent"
 					raised={ false }>
-					<Spinner className="w-6 mx-auto !stroke-gray-800 dark:!stroke-gray-200" />
 				</Button>
 			) : (
 				(isActive && status === "connected") ? (
