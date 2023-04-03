5bc2e2a98c7044e3b9c5a160cf70f288fe8545c7
Josh Merlino <joshua.merlino@gmail.com>
2023-04-03 14:13:07 -0400

    adjust shadow

--- src/renderer/components/Server.tsx
+++ src/renderer/components/Server.tsx
@@ -36,7 +36,7 @@ export default function Servers({ server }: { server: Ember.Server }): JSX.Eleme
 
 	// Render the server
 	return (
-		<div className="text-sm origin-top rounded-lg shadow dark:shadow-xl text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden dark:shadow-black/20 flex flex-col p-4 gap-2">
+		<div className="text-sm origin-top rounded-lg shadow text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden dark:shadow-black/20 flex flex-col p-4 gap-2">
 				
 			<div className="flex items-center gap-4 p-1">
 
