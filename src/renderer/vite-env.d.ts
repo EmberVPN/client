2978ff669e2e3df13ff44bfb1b4e42783098fb34
Josh Merlino <joshua.merlino@gmail.com>
2023-05-12 02:12:16 -0400

    get a good select plan screen working

--- src/renderer/vite-env.d.ts
+++ src/renderer/vite-env.d.ts
@@ -1,12 +1,8 @@
 /// <reference types="vite/client" />
 
-declare const electron: ElectronAPI;
+// Expose electron types
+type Electron = import("electron");
+declare const electron: Electron;
+
 declare const APIROOT: string;
 declare const VERSION: string;
-
-declare const config: {
-	get(key: string): unknown;
-	set(key: string, val: unknown): void;
-};
-
-declare const platform: string;
