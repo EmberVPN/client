795f14b9a1f9dd84981e257148d0fd727b64bdb6
Josh Merlino <joshua.merlino@gmail.com>
2023-05-12 00:30:49 -0400

    refactor deeper in the tree

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -7,6 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 import EntryPoint from "./Application";
 import Authorize from "./components/Auth/Login";
 import Titlebar from "./components/Titlebar";
+import Toolbar from "./components/Toolbar";
 import "./styles/index.less";
 import useConnection, { ConnectionProvider } from "./util/hooks/useConnection";
 import useData from "./util/hooks/useData";
@@ -52,29 +53,47 @@ export default function Application() {
 	// If the user is definitely not logged in, show the login screen
 	if (user === false) return (
 		<div className="flex flex-col h-screen bg-gray-100 dark:bg-gray-900">
+
+			{/* Window title bar */}
 			<Titlebar resizeable={ false }>Sign In</Titlebar>
+
+			{/* Login screen */}
 			<Authorize />
+
 		</div>
 	);
 	
 	// If we have all the data, render the app
 	if (user && ipLocation && data) return (
-		<div className="flex flex-col h-screen overflow-hidden">
+		<div className="flex flex-col w-screen h-screen overflow-hidden">
+
+			{/* Window title bar */}
 			<Titlebar className="bg-white dark:bg-gray-800" />
 			<div className="relative flex flex-col overflow-auto overflow-x-hidden select-none grow">
+
+				<Toolbar />
+
+				{/* Application */}
 				<EntryPoint />
+				
 			</div>
+			
 		</div>
 	);
 
 	// Otherwise, show the splash screen
 	return (
 		<div className="flex items-center justify-center w-full h-screen bg-white dark:bg-gray-800">
+
+			{/* Ripple background */}
 			<div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-				<div className="bg-primary aspect-square w-[400px] rounded-full animate-ember -z-10"></div>
+				<div className="bg-primary aspect-square w-[400px] rounded-full animate-ember -z-10" />
 			</div>
+
+			{/* Ember logo */}
 			<img className="z-10 select-none"
 				src={ favicon } />
+			
 		</div>
 	);
 }
