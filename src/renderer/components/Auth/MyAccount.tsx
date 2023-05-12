19dc4f0d00f262e5af3478ebb9160757e5f9ce75
Josh Merlino <joshua.merlino@gmail.com>
2023-05-12 19:29:40 -0400

    use user photo instead of more dots

--- src/renderer/components/Auth/MyAccount.tsx
+++ src/renderer/components/Auth/MyAccount.tsx
@@ -1,8 +1,7 @@
-import Icon from "@ui-elements/Icon";
 import Tooltip from "@ui-elements/Tooltip";
+import useRipple from "@ui-elements/util/useCenteredRipple";
 import classNames from "classnames";
-import { useState } from "react";
-import { MdMoreVert } from "react-icons/md";
+import { useRef, useState } from "react";
 import User from "../../util/class/User";
 import MyAccountPopout from "./MyAccountPopout";
 
@@ -11,17 +10,23 @@ export let close = () => { };
 
 export default function MyAccount({ user }: { user: User }): JSX.Element {
 
+	const ref = useRef<HTMLButtonElement>(null);
 	const [ _open, setOpen ] = useState(false);
 	open = () => setOpen(true);
 	close = () => setOpen(false);
+	useRipple(ref);
 
 	return (
 		<div className={ classNames("group relative", _open && "is-open") }>
-			<Icon className="group/tooltip"
-				icon={ MdMoreVert }
-				onClick={ open }>
+			<button className="flex mx-3 -my-1 text-sm rounded-full select-none bg-gray-500/10 md:mr-0 group/tooltip"
+				onClick={ open }
+				ref={ ref }>
+				<img
+					alt="user photo"
+					className="rounded-full w-9 h-9"
+					src={ User.getAvatarURL(user.id) } />
 				<Tooltip anchor="right">More</Tooltip>
-			</Icon>
+			</button>
 			<MyAccountPopout user={ user } />
 		</div>
 	);
