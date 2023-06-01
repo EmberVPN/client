37acca1b3b90949146bafd6b52514f5e5b956893
Josh Merlino <joshua.merlino@gmail.com>
2023-06-01 01:29:17 -0400

    allow installation of openvpn while unelevated

--- src/main/vpnutils.ts
+++ src/main/vpnutils.ts
@@ -1,19 +1,29 @@
-import { exec as oldExec } from "child_process";
+import admin from "admin-check";
+import child_process from "child_process";
 import { existsSync } from "fs";
-import { installPackage, isPackageInstalled } from "linux-package-manager";
 import { resolve } from "path";
-import { promisify } from "util";
+import sudoPrompt from "sudo-prompt";
 import { resources } from ".";
 
 // Install OpenVPN using the bundled installer
 export const BUNDLED_WIN32_INSTALLER = "OpenVPN-2.6.4-I001-amd64.msi";
 
-// Promisify exec with util.promisify
-const exec = (command: string) => promisify(oldExec)(command);
+// Promisify sudo-prompt & exec
+const sudo = (cmd: string) => new Promise<string | Buffer | undefined>((resolve, reject) => sudoPrompt.exec(cmd, { name: "Ember VPN" }, (error, stdout) => error ? reject(error) : resolve(stdout)));
+const exec = (cmd: string) => new Promise<string | Buffer | undefined>((resolve, reject) => child_process.exec(cmd, (error, stdout) => error ? reject(error) : resolve(stdout)));
 
-export async function getBinary() {
+/**
+ * A function to get the path to the OpenVPN binary, installing it if it doesn't exist
+ * @returns string
+ */
+export async function getBinary(): Promise<string> {
 
+	// Check platform
 	if (process.platform === "win32") {
+
+		// Check if openvpn is on path
+		const path = process.env.Path?.split(";").find(path => existsSync(resolve(path, "openvpn.exe")));
+		if (path) return resolve(path, "openvpn.exe");
 
 		// Check default location
 		const defaultLocation = resolve(process.env.ProgramFiles || "C:\\Program Files", "OpenVPN/bin/openvpn.exe");
@@ -23,28 +33,24 @@ export async function getBinary() {
 		const bundledLocation = resolve(resources, ".bin/bin/openvpn.exe");
 		if (existsSync(bundledLocation)) return bundledLocation;
 
-		// Set state to installing and install
-		await exec(`msiexec /i "${ resolve(resources, ".bin", BUNDLED_WIN32_INSTALLER) }" PRODUCTDIR="${ resolve(resources, ".bin") }" ADDLOCAL=OpenVPN.Service,Drivers.OvpnDco,OpenVPN,Drivers,Drivers.TAPWindows6,Drivers.Wintun /passive`);
+		// Check elevation status
+		const elevated = await admin.check();
 
-		// Return bundled location
-		return bundledLocation;
+		// Construct the install command
+		const cmd = `msiexec /i "${ resolve(resources, ".bin", BUNDLED_WIN32_INSTALLER) }" PRODUCTDIR="${ resolve(resources, ".bin") }" ADDLOCAL=OpenVPN.Service,Drivers.OvpnDco,OpenVPN,Drivers,Drivers.TAPWindows6,Drivers.Wintun /passive`;
 
-	}
+		// If we are not elevated, elevate
+		if (!elevated) await sudo(cmd);
+			
+		// If we are elevated, run the command
+		else await exec(cmd);
 
-	if (process.platform === "linux") {
-
-		// Check if openvpn is on path
-		const openvpn = await exec("which openvpn");
-		if (openvpn.stdout) return openvpn.stdout.toString().trim();
-
-		// Install OpenVPN if its not installed
-		if (!(await isPackageInstalled("openvpn"))) await installPackage("openvpn");
-
-		// Return openvpn
-		return "openvpn";
+		// // Return bundled location
+		return await getBinary();
 
 	}
-
+	
+	// Throw an error if the platform is not supported *yet*
 	throw new Error("Unsupported platform");
 
 }
