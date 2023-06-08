b23e48a0ecb71c81813643171a1751ee59dc1af7
Josh Merlino <joshua.merlino@gmail.com>
2023-06-08 01:42:46 -0400

    grammer police

--- src/renderer/windows/Update.tsx
+++ src/renderer/windows/Update.tsx
@@ -87,8 +87,8 @@ export function UpdateWindow(): JSX.Element {
 					{/* Update status */}
 					<MdBrowserUpdated className={ classNames("text-6xl shrink-0", outdated.length === 0 ? "text-success" : "text-warn") } />
 					<h1 className="text-2xl font-medium">{outdated.length === 0 ? "You're' up to date" : "Update found"}</h1>
-					<p className="mb-2 text-sm font-medium dark:font-normal opacity-60">
-						{outdated.length === 0 ? "You're running the latest version of Ember VPN." : "Updates found. Updating"}
+					<p className="mb-2 text-sm font-medium text-center dark:font-normal opacity-60">
+						{outdated.length === 0 ? "You're running the latest version of Ember VPN." : "Stay up to date with the latest features and enhancements by updating your EmberVPN client now." }
 					</p>
 				</div>
 					
