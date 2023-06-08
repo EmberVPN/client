e9464f0590e862f2ce8c9b89486352ad05c4cfe9
Josh Merlino <joshua.merlino@gmail.com>
2023-06-08 01:37:25 -0400

    change event

--- src/main/window/Update.ts
+++ src/main/window/Update.ts
@@ -17,23 +17,23 @@ class $Update extends Window {
 
 		// Get current version
 		const version = app.getVersion();
-
+		
 		// Get latest version
 		fetch("https://api.embervpn.org/v2/ember/downloads")
 			.then(res => res.json() as Promise<REST.APIResponse<EmberAPI.ClientDownloads>>)
 			.then(res => {
-
+			
 				// Make sure the request was successful
 				if (!res.success) return;
-
+			
 				// Get latest version
 				const latest = res.version.substring(1);
-
+				
 				// Compare versions
 				if (!gt(latest, version)) return;
 
 				// await for main window to load
-				app.once("ready", () => this.open());
+				app.once("browser-window-created", () => setTimeout(() => this.open(), 1000));
 				
 			});
 		
