d567ae7a167f625719538fb97661fa8acf5912d4
Josh Merlino <joshua.merlino@gmail.com>
2023-06-08 21:47:54 -0400

    start work on ssh looparound

--- src/renderer/windows/Settings.tsx
+++ src/renderer/windows/Settings.tsx
@@ -1,3 +1,4 @@
+import Checkbox from "@ui-elements/Checkbox";
 import useMounted from "@ui-elements/util/useMounted";
 import { ChangeEvent } from "react";
 import Titlebar from "../components/Titlebar";
@@ -9,6 +10,7 @@ export function SettingsWindow() {
 
 	// Get the current IP location
 	const { ipLocation } = useConnection();
+	const { status } = useConnection();
 	
 	// Adjust the window size
 	useMounted(() => electron.ipcRenderer.invoke("window-size", 600, 400));
@@ -19,6 +21,9 @@ export function SettingsWindow() {
 
 	// Get the theme
 	const [ theme, setTheme ] = useConfigKey<string>("theme");
+	
+	// Use looparound
+	const [ looparound, setLooparound ] = useConfigKey<boolean>("looparound");
 
 	// If location is still loading
 	if (!ipLocation) return null;
@@ -28,50 +33,68 @@ export function SettingsWindow() {
 			<Titlebar minimizeable={ false }
 				resizeable={ false }>Settings</Titlebar>
 			
-			<div className="flex flex-col gap-2 px-8 overflow-auto divide-y select-none grow divide-gray-20 dark:divide-gray-700/50">
+			<div className="flex flex-col px-8 overflow-auto divide-y select-none grow divide-gray-20 dark:divide-gray-700/50">
 
-				<section>
+				<section className="pb-4">
 					<h1 className="mt-6 text-2xl font-medium">App appearance</h1>
-					<div className="divide-y divide-gray-20 dark:divide-gray-700/50">
 
-						{/* Distance Units */}
-						<div className="flex items-center justify-between gap-4 py-2">
-							<div className="flex flex-col justify-center">
-								<h1 className="-mb-0.5 text-lg font-medium opacity-80">Distance Units</h1>
-								<p className="text-sm font-medium dark:font-normal opacity-60">Change the units used to display distances.</p>
-							</div>
-							<div className="pt-3 my-auto">
-								<DropDown
-									defaultValue={ imperial ? "Imperial (mi)" : "Metric (km)" }
-									label="Distance Units"
-									onChange={ (event: ChangeEvent<HTMLInputElement>) => setUnits(event.target.value === "Imperial (mi)" ? "IMPERIAL" : "METRIC") }
-									options={ [
-										"Metric (km)",
-										"Imperial (mi)"
-									] } />
-							</div>
+					{/* Distance Units */}
+					<div className="flex items-center justify-between h-20 gap-4 py-2 -mb-4">
+						<div className="flex flex-col justify-center">
+							<h1 className="-mb-0.5 text-lg font-medium opacity-80">Distance Units</h1>
+							<p className="text-sm font-medium dark:font-normal opacity-60">Change the units used to display distances.</p>
 						</div>
-						
-						{/* App theme */}
-						<div className="flex items-center justify-between gap-4 py-2">
-							<div className="flex flex-col justify-center">
-								<h1 className="-mb-0.5 text-lg font-medium opacity-80">App theme</h1>
-								<p className="text-sm font-medium dark:font-normal opacity-60">Change the theme used by the app.</p>
-							</div>
-							<div className="pt-3 my-auto">
-								<DropDown
-									defaultValue={ [ "LIGHT", "DARK" ].includes(theme) ? theme.toLowerCase().replace(/^[a-z]/, letter => letter.toUpperCase()) : "System (default)" }
-									label="App theme"
-									onChange={ (event: ChangeEvent<HTMLInputElement>) => setTheme(event.target.value.toUpperCase()) }
-									options={ [
-										"System (default)",
-										"Light",
-										"Dark"
-									] } />
-							</div>
+						<div className="pt-3 my-auto">
+							<DropDown
+								defaultValue={ imperial ? "Imperial (mi)" : "Metric (km)" }
+								label="Distance Units"
+								onChange={ (event: ChangeEvent<HTMLInputElement>) => setUnits(event.target.value === "Imperial (mi)" ? "IMPERIAL" : "METRIC") }
+								options={ [
+									"Metric (km)",
+									"Imperial (mi)"
+								] } />
 						</div>
-
 					</div>
+						
+					{/* App theme */}
+					<div className="flex items-center justify-between h-20 gap-4 py-2 -mb-4">
+						<div className="flex flex-col justify-center">
+							<h1 className="-mb-0.5 text-lg font-medium opacity-80">App theme</h1>
+							<p className="text-sm font-medium dark:font-normal opacity-60">Change the theme used by the app.</p>
+						</div>
+						<div className="pt-3 my-auto">
+							<DropDown
+								defaultValue={ [ "LIGHT", "DARK" ].includes(theme) ? theme.toLowerCase().replace(/^[a-z]/, letter => letter.toUpperCase()) : "System (default)" }
+								label="App theme"
+								onChange={ (event: ChangeEvent<HTMLInputElement>) => setTheme(event.target.value.toUpperCase()) }
+								options={ [
+									"System (default)",
+									"Light",
+									"Dark"
+								] } />
+						</div>
+					</div>
+
+				</section>
+
+				<section className="pb-4">
+					<h1 className="mt-6 text-2xl font-medium">Security</h1>
+
+					{/* Looparound */}
+					<label className="flex items-center justify-between h-20 gap-4 py-2 -mb-4"
+						htmlFor="opt_looparound">
+						<div className="flex flex-col justify-center">
+							<h1 className="-mb-0.5 text-lg font-medium opacity-80">SSH Looparound</h1>
+							<p className="text-sm font-medium dark:font-normal opacity-60">Add an extra layer of encryption via SSH tunneling.</p>
+						</div>
+						<div className="pt-3 my-auto">
+							<Checkbox defaultChecked={ looparound }
+								disabled={ status === "connecting" || status === "connected" || status === "disconnecting" || status === "will-connect" || status === "installing" }
+								id="opt_looparound"
+								onChange={ e => setLooparound(e.target.checked) } />
+						</div>
+					</label>
+
 				</section>
 
 			</div>
