d746cc756b9454a9d136e5f8074cc747c276ae7d
Josh Merlino <joshua.merlino@gmail.com>
2023-06-10 15:22:34 -0400

    support dependancies on usePromise

--- src/renderer/util/hooks/usePromise.tsx
+++ src/renderer/util/hooks/usePromise.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 
-export function usePromise<T>(promise: Promise<T>): T | undefined {
+export function usePromise<T>(promise: Promise<T>, dependencies = []): T | undefined {
 	const [ value, setValue ] = useState<T>();
 	useEffect(() => {
 		promise.then(setValue);
-	}, []);
+	}, dependencies);
 	return value;
 }
