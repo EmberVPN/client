7f278117822992558af6e419daabf55fe8210a30
Josh Merlino <joshua.merlino@gmail.com>
2023-06-09 20:43:24 -0400

    clean up the usermenu

--- src/renderer/components/MyAccount.tsx
+++ src/renderer/components/MyAccount.tsx
@@ -49,42 +49,45 @@ export function PopupWindow({ user }: { user: Auth.User }): JSX.Element {
 	}, []);
 
 	return (
-		<div className="absolute top-0 right-0 opacity-0 scale-75 group-[.is-open]:opacity-100 group-[.is-open]:scale-100 transition-all origin-top-right rounded-2xl p-4 min-w-full w-[320px] pointer-events-none group-[.is-open]:pointer-events-auto shadow-xl border dark:border-gray-700/50 text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden -m-2"
+		<div
+			className="absolute top-0 right-0 opacity-0 scale-75 group-[.is-open]:opacity-100 group-[.is-open]:scale-100 transition-all origin-top-right rounded-2xl p-4 min-w-full w-[320px] pointer-events-none group-[.is-open]:pointer-events-auto dark:shadow-xl dark:shadow-black/20 border dark:border-gray-700/50 text-gray-600 bg-white dark:bg-gray-800 dark:text-gray-400 overflow-hidden ease-bounce duration-[300ms] drop-shadow-xl"
 			onClick={ e => e.stopPropagation() }>
 			<div className="flex items-center gap-4 overflow-hidden">
-				<img alt={ user.username }
+				<img
+					alt={ user.username }
 					className="w-16 h-16 my-auto rounded-full bg-gray-500/20"
 					src={ User.getAvatarURL(user.id) } />
 				<div className="flex flex-col pr-4 overflow-hidden text-sm text-left grow">
-					<span className="flex items-center gap-4 text-lg font-medium text-gray-800 truncate dark:text-gray-200">
-						{user.username}
-					</span>
+					<span className="flex items-center gap-4 text-lg font-medium text-gray-800 truncate dark:text-gray-200">{user.username}</span>
 					<span className="truncate">{user.email}</span>
 					<span className="text-xs truncate">Member since <span className="font-medium">{dayjs(user.created_ms).format("MM/DD/YYYY")}</span></span>
-					
 				</div>
 			</div>
 			<div className="flex flex-col mt-4 -m-2">
 				
-				<DrawerItem icon={ MdOutlineBrowserUpdated }
+				<DrawerItem
+					icon={ MdOutlineBrowserUpdated }
 					onClick={ () => [ close(), electron.ipcRenderer.send("open", "updater") ] }
 					size="dense">
 					Check for updates
 				</DrawerItem>
 				
-				<DrawerItem icon={ MdManageAccounts }
+				<DrawerItem
+					icon={ MdManageAccounts }
 					onClick={ () => [ close(), window.open("https://embervpn.org/my/account") ] }
 					size="dense">
 					My account
 				</DrawerItem>
 				
-				<DrawerItem icon={ IoMdSettings }
+				<DrawerItem
+					icon={ IoMdSettings }
 					onClick={ () => [ close(), electron.ipcRenderer.send("open", "settings") ] }
 					size="dense">
 					Settings
 				</DrawerItem>
 				
-				<DrawerItem icon={ MdExitToApp }
+				<DrawerItem
+					icon={ MdExitToApp }
 					onClick={ () => [ close(), electron.ipcRenderer.send("authorization", null) ] }
 					size="dense">
 					Sign out
