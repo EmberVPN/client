94b49819f7cfebea1cd2f055cabb5ecf2d0a46f0
Josh Merlino <joshua.merlino@gmail.com>
2023-06-09 22:42:12 -0400

    whoops

--- src/renderer/util/hooks/useConfigKey.ts
+++ src/renderer/util/hooks/useConfigKey.ts
@@ -4,14 +4,14 @@ import { ConfigType } from "../../../main/class/Config";
 export function useConfigKey<T extends keyof ConfigType>(key: T) {
 
 	// Initialize state
-	const state = useState<T>(electron.ipcRenderer.sendSync("config", key));
+	const state = useState<ConfigType[T]>(electron.ipcRenderer.sendSync("config", key));
 	
 	// Observe state changes
 	useEffect(function() {
 		
 		// Listen for config updates
 		electron.ipcRenderer.on("config-updated", function(_, k: string, value: unknown) {
-			if (key === k) state[1](value as T);
+			if (key === k) state[1](value as ConfigType[T]);
 		});
 
 	}, [ key, state ]);
