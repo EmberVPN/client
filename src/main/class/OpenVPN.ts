45a4897460dd812353349df5694a0fecd0d9cda7
Josh Merlino <joshua.merlino@gmail.com>
2023-06-14 21:15:09 -0400

    prevent ovpn uninstall when update

--- src/main/class/OpenVPN.ts
+++ src/main/class/OpenVPN.ts
@@ -1,10 +1,10 @@
 import admin from "admin-check";
 import { ChildProcess, exec, spawn } from "child_process";
-import { BrowserWindow, app, ipcMain } from "electron";
+import { BrowserWindow, ipcMain } from "electron";
 import { existsSync } from "fs";
 import { writeFile } from "fs/promises";
 import os from "os";
-import { dirname, extname, join, resolve } from "path";
+import { extname, join, resolve } from "path";
 import { SemVer, coerce } from "semver";
 import { resources } from "..";
 import { calculateDistance } from "../../calculateDistance";
@@ -313,10 +313,6 @@ export class OpenVPN {
 			// Check default location
 			const defaultLocation = resolve(process.env.ProgramFiles || "C:\\Program Files", "OpenVPN/bin/openvpn.exe");
 			if (existsSync(defaultLocation)) return defaultLocation;
-
-			// Check bundled location
-			const bundledLocation = resolve(dirname(app.getPath("exe")), "bin/openvpn.exe");
-			if (existsSync(bundledLocation)) return bundledLocation;
 		
 			if (installIfMissing) {
 				
@@ -369,7 +365,7 @@ export class OpenVPN {
 				.then(buffer => writeFile(savePath, Buffer.from(buffer)));
 
 			// Install openvpn
-			return await new Promise(resolve => exec(`msiexec /i "${ savePath }" PRODUCTDIR="${ dirname(app.getPath("exe")) }" ADDLOCAL=OpenVPN.Service,Drivers.OvpnDco,OpenVPN,Drivers,Drivers.TAPWindows6,Drivers.Wintun /passive`, resolve));
+			return await new Promise(resolve => exec(`msiexec /i "${ savePath }" ADDLOCAL=OpenVPN.Service,Drivers.OvpnDco,OpenVPN,Drivers,Drivers.TAPWindows6,Drivers.Wintun /passive`, resolve));
 
 		}
 
