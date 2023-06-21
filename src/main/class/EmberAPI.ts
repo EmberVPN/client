e61c92936fafdd3dc5ce3ace20db014e39e6fbb2
Josh Merlino <joshua.merlino@gmail.com>
2023-06-20 20:15:46 -0400

    cache and dedupe support

--- src/main/class/EmberAPI.ts
+++ src/main/class/EmberAPI.ts
@@ -16,13 +16,36 @@ export class EmberAPI {
 	// The current authorization token
 	protected static authorization = Config.get("auth.token") || undefined;
 
+	// A registry of requests currently inflight for deduping
+	private static inflight = {} as Record<`${ string } ${ keyof IdealResponses }`, { ts: number, d: IdealResponses[keyof IdealResponses] } | true>;
+
 	/**
-	 * Fetches data from the Ember API
+	 * Fetches data from the Ember API. Responses are automatically deduped.
 	 * @param path The path to fetch from (if not absolute, will be appended to the API root)
 	 * @returns Promise<IdealResponse[T]>
 	 */
 	public static async fetch<T extends keyof IdealResponses>(path: T, init?: Partial<RequestInit>) {
 
+		// Attempt to dedupe
+		const key = `${ init?.method || "GET" } ${ path }`;
+		
+		// If we have a cached response & its not stale, return it
+		if (EmberAPI.inflight[key] && EmberAPI.inflight[key] !== true) {
+			if (Date.now() - EmberAPI.inflight[key].ts > 1000) delete EmberAPI.inflight[key];
+			else return EmberAPI.inflight[key].d;
+		}
+
+		// If were currently inflight, just wait for that to finish
+		if (EmberAPI.inflight[key] === true) {
+
+			// Wait for the request to finish
+			while (EmberAPI.inflight[key] === true) await new Promise(r => setTimeout(r, 10));
+
+			// Return the cached response
+			return EmberAPI.inflight[key].d;
+
+		}
+		
 		// Make sure the path is not outside of the API root domain
 		if (path.startsWith("http") && !path.startsWith(EmberAPI.URL)) throw new Error("Path cannot be absolute. Start with a slash to make it relative to the API root.");
 
@@ -34,13 +57,25 @@ export class EmberAPI {
 		// If we have an authorization token, add it to the headers
 		if (EmberAPI.authorization) init.headers["Authorization"] = EmberAPI.authorization;
 
+		// Set the inflight state
+		EmberAPI.inflight[key] = true;
+
+		console.log(`[FETCH] ${ key }`);
+
 		// Fetch the data
 		const response = await fetch(EmberAPI.URL.concat(path), init)
-			.then(res => res.json() as Promise<REST.APIResponse<IdealResponses[T]>>);
+			.then(res => res.json() as Promise<REST.APIResponse<IdealResponses[T]>>)
+			.finally(() => delete EmberAPI.inflight[key]);
 		
 		// If the response is an error, throw it
 		if (!response.success) throw new Error(response.readable || response.description || response.error);
-		
+
+		// Cache the response
+		EmberAPI.inflight[key] = {
+			ts: Date.now(),
+			d: response
+		};
+
 		// Return the response
 		return {
 			...response,
